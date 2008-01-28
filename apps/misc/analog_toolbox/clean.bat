b7f24333656c6ed6266162a00feaec9028841609
tk <tk@7895a35d-cd44-0410-bfa6-c66f5b1fdf0a>
2008-01-28 21:21:48 +0000

    changed makefile variable names

--- apps/misc/analog_toolbox/clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Removing temporary files
-
-if exist _output del /s /q _output
-if exist project.cod del /q project.cod
-if exist project.map del /q project.map
-if exist project.lst del /q project.lst
-
