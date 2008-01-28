473e98e5164d800af6cebea5b24d5cb6557ed251
tk <tk@7895a35d-cd44-0410-bfa6-c66f5b1fdf0a>
2008-01-28 21:21:19 +0000

    changed makefile variable names

--- include/makefile/common.mk
+++ include/makefile/common.mk
@@ -1,21 +1,46 @@
 
+# default project name
 PROJECT=project
+
+# output directory
 OUTDIR=_output
 
+# GPASM execution via wrapper
 GPASM=perl $(MIOS_BIN_PATH)/mios-gpasm.pl -c -o $(OUTDIR)/
-CC=perl $(MIOS_BIN_PATH)/mios-sdcc.pl -c -o $(OUTDIR)/
+
+# SDCC execution via wrapper
+SDCC=perl $(MIOS_BIN_PATH)/mios-sdcc.pl -c -o $(OUTDIR)/
+
+# GPLINK execution (w/o wrapper yet)
 GPLINK=gplink
 
-GPASMFLAGS=-p $(PROCESSOR) $(ASM_INCLUDE) -DDEBUG_MODE=0
-CCFLAGS=-mpic16 -$(PROCESSOR) $(CC_INCLUDE) --fommit-frame-pointer --optimize-goto --optimize-cmp --disable-warning 85 --obanksel=2 -pleave-reset-vector
+# default flags for GPASM
+GPASM_FLAGS=-p $(PROCESSOR) $(GPASM_INCLUDE) -DDEBUG_MODE=0
 
+# default flags for SDCC
+SDCC_FLAGS=-mpic16 -$(PROCESSOR) $(SDCC_INCLUDE) --fommit-frame-pointer --optimize-goto --optimize-cmp --disable-warning 85 --obanksel=2 -pleave-reset-vector
+
+# rule to create a .hex file
+# note: currently we always require a "cleanall", since dependencies (e.g. on .h files) are not properly managed
+# later we could try it w/o "cleanall", and propose the usage of this step to the user
 $(PROJECT).hex: cleanall mk_outdir $(addprefix $(OUTDIR)/, $(OBJS))
-	$(GPLINK) $(GPLINKFLAGS) -m -o $(PROJECT).hex $(addprefix $(OUTDIR)/, $(OBJS))
+	$(GPLINK) $(GPLINK_FLAGS) -m -o $(PROJECT).hex $(addprefix $(OUTDIR)/, $(OBJS))
 
+# default rule for compiling .c programs
 $(OUTDIR)/%.o: %.c
-	$(CC) $(CCFLAGS) $(SDCC_DEFINES) $< -o $(OUTDIR)/
+	$(SDCC) $(SDCC_FLAGS) $(SDCC_DEFINES) $<
 
+# default rule for compiling .asm programs
+# we expect, that they don't need to be converted through the wrapper
+$(OUTDIR)/%.o: %.asm
+	$(GPASM) $(GPASM_FLAGS) $<
 
+# default rule for compiling .s programs (these are assembler programs as well)
+# for .s we expect, that they need to be converted through the wrapper
+$(OUTDIR)/%.o: %.s
+	$(GPASM) $(GPASM_FLAGS) $<
+
+# include mios_wrapper related rules
 include $(MIOS_PATH)/modules/mios_wrapper/mios_wrapper.mk
 
 # add makefile rules of supported processors here
